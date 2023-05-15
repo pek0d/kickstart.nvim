6d1d4fc813d021b4bcc5471b69869ac9265d937a
pekod <onathema@gmail.com>
2023-05-15 14:09:13 +0300

    colorsschemes in separate files

---
+++ lua/custom/plugins/gruvbox.lua
@@ -0,0 +1,33 @@
+return
+-- colorscheme gruvbox
+{
+  'ellisonleao/gruvbox.nvim',
+  lazy = true,         -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000,     -- make sure to load this before all the other start plugins
+  config = function()
+    require("gruvbox").setup({
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true,     -- invert background for search, diffs, statuslines and errors
+      contrast = "",      -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = true,
+      transparent_mode = false,
+    })
+    -- load the colorscheme here
+    -- vim.cmd.colorscheme "gruvbox"
+  end
+}
