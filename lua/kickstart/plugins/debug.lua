c1c26ceb58340225a244080d284550669d1c7b9c
pekod <onathema@gmail.com>
2023-08-26 00:49:39 +0300

    iron repl added

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -27,44 +27,21 @@ return {
     local dap = require 'dap'
     local dapui = require 'dapui'
 
+
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
       automatic_setup = true,
+
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {},
 
       -- You'll need to check that you have the required things installed
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'python',
-        handlers = {
-          function(config)
-            -- all sources with no handler get passed here
-
-            -- Keep original functionality
-            require('mason-nvim-dap').default_setup(config)
-          end,
-          python = function(config)
-            config.adapters = {
-              type = "executable",
-              command = "/Library/Frameworks/Python.framework/Versions/3.11/bin/python3",
-              args = {
-                "-m",
-                "debugpy.adapter",
-              },
-            }
-            require('mason-nvim-dap').default_setup(config) -- don't forget this!
-          end,
-        },
-      }
-    }
-
-    dap.configurations.python = {
-      {
-        type = 'python',
-        request = 'launch',
-        name = "Launch file",
-        program = "${file}",
       },
     }
 
