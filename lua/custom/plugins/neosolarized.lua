907e0e8e6994e60f011b3678ec7e1719d284eecd
Марк Рубцов <mark@Mac-Pro.local>
2023-08-26 23:20:01 +0300

    some tune for neosolarized theme

--- lua/custom/plugins/neosolarized.lua
+++ lua/custom/plugins/neosolarized.lua
@@ -6,13 +6,13 @@ return
 	config = function()
 		require('NeoSolarized').setup {
 			style = 'dark', -- "dark" or "light"
-			transparent = true, -- true/false; Enable this to disable setting the background color
+			transparent = false, -- true/false; Enable this to disable setting the background color
 			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
 			enable_italics = true, -- Italics for different hightlight groups (eg. Statement, Condition, Comment, Include, etc.)
 			styles = {
 				-- Style to be applied to different syntax groups
 				comments = { italic = true },
-				keywords = { italic = true },
+				keywords = { italic = false },
 				functions = { bold = true },
 				variables = {},
 				string = { italic = true },
