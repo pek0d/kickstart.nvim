53022b64bd9f1e9b72ef87e7abaf8ef0148532d6
pekod <pekod@Mac-Pro-pekod.local>
2023-10-08 00:19:07 +0300

    added auto-dark mode and removed neosolarized

---
+++ lua/custom/plugins/auto-dark-mode.lua
@@ -0,0 +1,16 @@
+return
+-- auto-dark-mode
+{
+	"f-person/auto-dark-mode.nvim",
+	config = {
+		update_interval = 1000,
+		set_dark_mode = function()
+			vim.api.nvim_set_option("background", "dark")
+			vim.cmd("colorscheme gruvbox")
+		end,
+		set_light_mode = function()
+			vim.api.nvim_set_option("background", "light")
+			vim.cmd("colorscheme onedark")
+		end,
+	},
+}
