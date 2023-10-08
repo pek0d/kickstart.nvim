f1d2c6653c1e6d9a39480abfc57a7e801da27999
pekod <onathema@gmail.com>
2023-10-08 18:13:24 +0300

    switched to onedark

--- lua/custom/plugins/auto-dark-mode.lua
+++ lua/custom/plugins/auto-dark-mode.lua
@@ -6,7 +6,7 @@ return
 		update_interval = 1000,
 		set_dark_mode = function()
 			vim.api.nvim_set_option("background", "dark")
-			vim.cmd("colorscheme gruvbox")
+			vim.cmd("colorscheme onedark")
 		end,
 		set_light_mode = function()
 			vim.api.nvim_set_option("background", "light")
